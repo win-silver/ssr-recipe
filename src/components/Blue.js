247e4c2fcc728d96c4900a139dec754ea7ea8e62
seungeun_yeon <seungeun_yeon@tmax.co.kr>
2021-07-23 18:12:30 +0900

    feat: first commit

---
+++ src/components/Blue.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Blue.css';
+
+const Blue = () => {
+  return <div classname="Blue">Blue</div>;
+}
+
+export default Blue;
---
+++ src/components/Red.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Red.css';
+
+const Red = () => {
+  return <div clasName="Red">Red</div>
+}
+
+export default Red;
